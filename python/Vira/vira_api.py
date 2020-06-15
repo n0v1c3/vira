307e79cc15f468c8cb3867080d1709ab5caebfa7
Travis Gall <travis.gall@gmail.com>
2020-06-14 20:41:39 -0600

    VIRA-177: issues menu layout update

--- python/Vira/vira_api.py
+++ python/Vira/vira_api.py
@@ -244,14 +244,53 @@ class ViraAPI():
         Get my issues with JQL
         '''
 
+        keys = []
+        summary_list = []
+        issuetype_list = []
+        status_list = []
+        user_list = []
+        key_length = 0
+        summary_length = 0
+        issuetype_length = 0
+        status_length = 0
+        user_length = 0
+
         for issue in self.query_issues():
-            user = str(issue['fields']['assignee']['displayName']) if type(
-                issue['fields']['assignee']) == dict else 'Unassigned'
+            keys.append(issue['key'])
+            key_length = len(issue['key']) if len(
+                issue['key']) > key_length else key_length
+
+            fields = issue['fields']
+
+            summary = fields['summary']
+            summary_list.append(summary)
+            summary_length = len(summary) if len(summary
+                    ) > summary_length else summary_length
+
+            issuetype = fields['issuetype']['name']
+            issuetype_list.append(issuetype)
+            issuetype_length = len(issuetype) if len(issuetype
+                    ) > issuetype_length else issuetype_length
+
+            status = fields['status']['name']
+            status_list.append(status)
+            status_length = len(status) if len(status
+                    ) > status_length else status_length
+
+            user = str(fields['assignee']['displayName']) if type(
+                fields['assignee']) == dict else 'Unassigned'
+            user_list.append(user)
+            user_length = len(user) if len(user
+                    ) > user_length else user_length
+
+        i = 0
+        for key in keys:
             print(
-                issue["key"] + "  ~  " + issue["fields"]["summary"] + " |  " +
-                issue["fields"]["issuetype"]["name"] + " - " +
-                issue["fields"]["status"]["name"] +
-                '  ->  ' + user)
+                ('{: <' + str(key_length) + '}').format(key
+                    ) + "  ~  " + ('{: <' + str(summary_length) + '}').format(summary_list[i])
+                + " |  " + ('{: <' + str(issuetype_length) + '}').format(issuetype_list[i]
+                    ) + " - " + ('{: <' + str(status_length) + '}').format(status_list[i]) + '  ->  ' + user_list[i])
+            i = i + 1
 
     def get_issuetypes(self):
         '''
